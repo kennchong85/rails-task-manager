c6e07eb6243f4274cee38a12cc71fbde0cfbd032
kennchong <kennchong85@gmail.com>
2020-03-10 20:07:59 +0800

    completed the 7 CRUDs action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,11 +18,34 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(user_params) # We'll see that in a moment.
+
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    # no need for app/views/tasks/destroy.html.erb
+    redirect_to tasks_path
+  end
+
   private
 
   def task_params
     # *Strong params*: You need to *whitelist* what can be updated by the user
     # Never trust user data!
     params.require(:task).permit(:title, :details)
+  end
+
+  def user_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
